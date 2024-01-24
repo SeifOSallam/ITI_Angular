d3d05da393679202808baec718df7dca308e20ed
SeifOSallam <seif.osallam@gmail.com>
2024-01-25 00:13:34 +0200

    Lab 1

--- Lab_1/src/app/app.component.ts
+++ Lab_1/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent, AboutmeComponent, SkillsComponent, ProjectsComponent],
+  imports: [RouterOutlet, HeroComponent, AboutmeComponent, SkillsComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
